a5695ff91acb48328642b8c0a58f5f442c74d236
Mounir Nessim <mmnessim@gmail.com>
2025-02-26 06:42:13 -0500

    post requests and testing

--- src/main.zig
+++ src/main.zig
@@ -14,8 +14,25 @@ pub fn main() !void {
     const fullURL = try ztring.concatString(baseURL, username, &buffer);
 
     var headerBuf: [1024 * 1024]u8 = undefined;
-    var bodyBuf: [1024 * 1024]u8 = undefined;
+    var bodyBuf: [4096 * 1024]u8 = undefined;
+    const postBody = "Hello from Zig";
 
     const result = try simple_http.GET(fullURL, allocator, &headerBuf, &bodyBuf);
     std.debug.print("RESULT: {s}\n", .{result});
+
+    const echoURL = "http://postman-echo.com/post";
+
+    const postResult = simple_http.POST(echoURL, postBody, allocator, &headerBuf, &bodyBuf) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        return;
+    };
+    std.debug.print("RESULT: {s}\n", .{postResult});
+}
+
+fn getLine(buffer: []u8) ![]const u8 {
+    const stdin = std.io.getStdIn().reader();
+
+    const line = try stdin.readUntilDelimiter(buffer, '\n');
+
+    return line;
 }
--- src/root.zig
+++ src/root.zig
@@ -18,8 +18,42 @@ pub fn GET(url: []const u8, alloc: std.mem.Allocator, headerBuffer: []u8, bodyBu
 
     std.debug.print("status={d}\n", .{req.response.status});
 
-    const bodyLen = try req.reader().readAll(bodyBuffer);
-    const result: []const u8 = bodyBuffer[0..bodyLen];
+    const bodyLen = req.reader().readAll(bodyBuffer) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        return err;
+    };
 
+    const result: []const u8 = bodyBuffer[0..bodyLen];
+    return result;
+}
+
+pub fn POST(url: []const u8, reqBody: []const u8, alloc: std.mem.Allocator, headerBuffer: []u8, bodyBuffer: []u8) ![]const u8 {
+    var client = std.http.Client{ .allocator = alloc };
+    defer client.deinit();
+
+    const uri = try std.Uri.parse(url);
+    var req = client.open(.POST, uri, .{
+        .server_header_buffer = headerBuffer,
+        .headers = .{ .user_agent = .{ .override = USERAGENT }, .content_type = .{ .override = "text/plain" } },
+    }) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        return err;
+    };
+    defer req.deinit();
+
+    req.transfer_encoding = .{ .content_length = reqBody.len };
+
+    try req.send();
+    try req.writer().writeAll(reqBody);
+    try req.finish();
+    try req.wait();
+
+    std.debug.print("status={d}\n", .{req.response.status});
+
+    const bodyLen = req.reader().readAll(bodyBuffer) catch |err| {
+        std.debug.print("Error: {}\n", .{err});
+        return err;
+    };
+    const result = bodyBuffer[0..bodyLen];
     return result;
 }
